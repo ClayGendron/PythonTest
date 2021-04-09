e637723a5e48b3fb5fe53023e50d236c7309532e
harvey39 <c.gendron1@snhu.edu>
2021-04-09 18:33:45 -0400

    commiter

--- PythonTestScript.py
+++ PythonTestScript.py
@@ -5,3 +5,13 @@ print("This is from Windows")
 print("This is from Mac")
 
 print("Mac is Better Than Windows")
+
+print("    /\    ")
+print("   /  \   ")
+print("  /    \  ")
+print(" /______\ ") 
+
+charecter_name = "John"
+charecter_age = "70"
+
+print("There onces was a man named " + charecter_name + " who was the age of " + charecter_age + ".")
